e72e766cdb814b2bcc4d915e47d6c201d178ccf9
Marie Sandler <marie.sandler@mail.mcgill.ca>
2026-04-07 14:14:56 -0400

    Initial regularized entropy set up - not tested

--- torchcfm/prior.py
+++ torchcfm/prior.py
@@ -1,9 +1,11 @@
 import numpy as np
+import ot as pot
 
 def prior_ot_fn(
     a,
     b,
     M,
+    reg,
     prior_method='to_first'
 ):
     '''
@@ -17,16 +19,32 @@ def prior_ot_fn(
 
     These should be taking as argument and outputting numpy arrays.
     '''
+    Q = np.zeros((a.shape[0], b.shape[0])) 
+    # Prior cost matrix, which can be used to encode the prior information about the transport plan.
+    if prior_method == 'basic_entropic_ot':
+        Q = basic_entropic_ot_plan(a, b)
     if prior_method == 'to_first':
-        return to_first_ot_plan(a, b)
+        Q = to_first_ot_plan(a, b)
+    
+    #Ensure no zero entries in Q to avoid log(0) issues, and normalize Q to be a valid probability distribution.
+    eps = 1e-8
+    Q = np.maximum(Q, eps)
+    Q = Q / Q.sum()
+    
+    M_adjusted = M - reg * np.log(Q)
 
-    raise ValueError(f'Prior method {prior_method} not implemented')
+    #raise ValueError(f'Prior method {prior_method} not implemented')
+    return pot.sinkhorn(a, b, M_adjusted, reg=reg)
 
 def to_first_ot_plan(a, b):
     '''
     Example OT method where we simply force it all to the very first cell in b.
     i.e. this is [1, 0, ..., 0] where these are represented as column vectors.
     '''
-    G = np.zeros((a.shape[0], b.shape[0]))
-    G[:, 0] = 1
-    return G
+    Q = np.zeros((a.shape[0], b.shape[0]))
+    Q[:, 0] = 1
+    return Q
+
+def basic_entropic_ot_plan(a, b):
+    Q = np.outer(a,b)
+    return Q
